c77af01c789619e293cf5209d8911f033a9ccc14
Sergey Emelyanov <karagabul@gmail.com>
2021-10-15 06:48:55 +0300

    Add base frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <MainMap/>
+  </div>
+</template>
+
+<script>
+import MainMap from './components/MainMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainMap
+  }
+}
+</script>
+
+<style>
+body, html {
+  height: 100%;
+  width: 100%;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
