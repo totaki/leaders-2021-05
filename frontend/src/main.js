0e4de5877779f2a998bd1cb581c916367edc04ce
doker696 <doker696@users.noreply.github.com>
2021-10-16 12:20:04 +0300

    fixed marker icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from "./core/store.js"
 import vuetify from '@/plugins/vuetify'
+import { Icon } from 'leaflet';
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,10 @@ new Vue({
   vuetify,
   render: h => h(App),
 }).$mount('#app')
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
