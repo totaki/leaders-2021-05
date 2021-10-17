faa8d9ff9e7d10e1a82994f618444825667d9eff
andrew <off-rey@bk.ru>
2021-10-18 00:17:03 +0300

    Add tiles and availability filters to facility entrypoint.

--- backend/facilities/filters.py
+++ backend/facilities/filters.py
@@ -1,12 +1,13 @@
 from django_filters import rest_framework as filters
 
-from facilities.models import Facility
+from facilities.models import Facility, AvailabilityType
 
 
 class FacilityFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr='icontains')
     area_name = filters.CharFilter(field_name="areas__name", lookup_expr='icontains')
     area_type = filters.BaseInFilter(field_name="area_types", lookup_expr='contains')
+    availability = filters.MultipleChoiceFilter(field_name="availability", choices=AvailabilityType.choices)
 
     class Meta:
         model = Facility
--- backend/facilities/views.py
+++ backend/facilities/views.py
@@ -1,10 +1,13 @@
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Sum
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters import rest_framework as filters
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework_gis.filters import TMSTileFilter
 
 from facilities.filters import FacilityFilter
 from facilities.models import (
@@ -36,8 +39,10 @@ class FacilitiesViewSet(ReadOnlyModelViewSet):
         .all()
     )
     serializer_class = FacilitySerializer
-    filter_backends = [filters.DjangoFilterBackend]
+    filter_backends = [filters.DjangoFilterBackend, TMSTileFilter]
     filterset_class = FacilityFilter
+    bbox_filter_include_overlapping = True
+    bbox_filter_field = 'placement'
     pagination_class = LimitOffsetPagination
 
     @action(detail=True)
@@ -52,6 +57,10 @@ class FacilitiesViewSet(ReadOnlyModelViewSet):
                 area_types=ArrayAgg("areas__type", distinct=True)
             )
         return super(FacilitiesViewSet, self).get_queryset()
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    def list(self, request, *args, **kwargs):
+        return super(FacilitiesViewSet, self).list(request, *args, **kwargs)
 
 
 class SportTypesViewSet(ReadOnlyModelViewSet):
